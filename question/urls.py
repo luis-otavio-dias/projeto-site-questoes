7c753546beebdf41b155f35c03ac5b6fb8dfda43
luis-otavio-dias <luis777dias231@gmail.com>
2025-03-15 20:44:18 -0300

    Configurando view e url single_question, templates do app, templates globais e css

--- question/urls.py
+++ question/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from question.views import index
+from question import views
 
 app_name = "question"
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", views.index, name="index"),
+    path("question/<int:question_id>/", views.question, name="question"),
 ]
--- question/views.py
+++ question/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from question.models import Question
 
 
@@ -8,10 +8,27 @@ def index(request):
 
     context = {
         "questions": questions,
+        "site_title": "Title - ",
     }
 
     return render(
         request,
         "question/index.html",
+        context,
+    )
+
+
+def question(request, question_id):
+    single_question = get_object_or_404(Question, pk=question_id)
+    site_title = f"{single_question.id}"
+
+    context = {
+        "single_question": single_question,
+        "site_title": site_title,
+    }
+
+    return render(
+        request,
+        "question/single_question.html",
         context,
     )
