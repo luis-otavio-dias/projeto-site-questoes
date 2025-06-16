8d379a4c023d5862610659b3f40d17558f7d2d2c
luis-otavio-dias <luis777dias231@gmail.com>
2025-06-16 16:13:13 -0300

    Criando app home para homepage

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("project.apps.user.urls")),
     path("", include("project.apps.question.urls")),
+    path("", include("project.apps.home.urls")),
     path("admin/", admin.site.urls),
 ]
 
