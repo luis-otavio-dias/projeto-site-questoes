8acad9f5c1efbb635387a34661ac5ba351dc5007
luis-otavio-dias <luis777dias231@gmail.com>
2026-01-05 21:10:27 -0300

    Atualiza MainTemplate para utilizar UploadModal

--- frontend/src/templates/MainTemplate/index.tsx
+++ frontend/src/templates/MainTemplate/index.tsx
@@ -1,6 +1,8 @@
 import { SideBar } from "../../components/SideBar";
 import { DefaultMenu } from "../../components/DefaultMenu";
 import { cn } from "../../lib/utils";
+import { UploadModal } from "../../components/UploadModal";
+import { useState } from "react";
 
 type MainTemplateProps = {
   children: React.ReactNode;
@@ -8,10 +10,18 @@ type MainTemplateProps = {
 };
 
 export function MainTemplate({ children, className }: MainTemplateProps) {
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState<boolean>(false);
+
   return (
     <div className="min-h-screen w-full grid grid-cols-[auto_1fr] grid-rows-[auto_1fr]">
       <DefaultMenu />
-      <SideBar />
+      <SideBar onOpenUploadModal={() => setIsUploadModalOpen(true)} />
+
+      <UploadModal
+        isOpen={isUploadModalOpen}
+        onClose={() => setIsUploadModalOpen(false)}
+      />
+
       <main className={cn("col-span-5", className)}>
         <div className="w-full grid grid-cols-[auto_1fr] grid-rows-[90vh]">
           <div className="col-span-full row-span-full ">{children}</div>
