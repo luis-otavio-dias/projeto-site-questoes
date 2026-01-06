66b1b61bd80d2a2bb2e8ff016c3b2720b379308e
luis-otavio-dias <luis777dias231@gmail.com>
2026-01-05 21:09:50 -0300

    Atualiza componentes SideBar e SideBarItem para suportar interação com modal

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -11,26 +11,30 @@ import { SideBarSection } from "../SideBarSection";
 import { SideBarItem } from "../SideBarItem";
 import { SideBarUserItem } from "../SideBarUserItem";
 
-export function SideBar() {
+type SideBarProps = {
+  onOpenUploadModal?: () => void;
+};
+
+export function SideBar({ onOpenUploadModal }: SideBarProps) {
   return (
     <div className="max-h-screen w-75 border-r-2 grid grid-rows-[auto_auto_auto_auto_1fr] gap-4 content-start py-4">
       <SideBarSection title="Meus Estudos">
-        <SideBarItem icon={LucidePlus} to="/upload_file" text="Adicionar" />
         <SideBarItem
-          icon={ChartColumn}
-          to="/estatisticas"
-          text="Estatísticas"
+          icon={LucidePlus}
+          text="Adicionar"
+          onClick={onOpenUploadModal}
         />
-        <SideBarItem icon={HeartIcon} to="/favoritos" text="Favoritos" />
-        <SideBarItem icon={HistoryIcon} to="/historico" text="Histórico" />
+        <SideBarItem icon={ChartColumn} to="#" text="Estatísticas" />
+        <SideBarItem icon={HeartIcon} to="#" text="Favoritos" />
+        <SideBarItem icon={HistoryIcon} to="#" text="Histórico" />
       </SideBarSection>
 
       <hr className="border-1" />
 
       <SideBarSection title="Filtrar por">
-        <SideBarItem icon={LucidePuzzle} to="/filtro/materia" text="Matéria" />
-        <SideBarItem icon={Calendar} to="/filtro/ano" text="Ano" />
-        <SideBarItem icon={NotebookPen} to="/filtro/prova" text="Prova" />
+        <SideBarItem icon={LucidePuzzle} to="#" text="Matéria" />
+        <SideBarItem icon={Calendar} to="#" text="Ano" />
+        <SideBarItem icon={NotebookPen} to="#" text="Prova" />
       </SideBarSection>
 
       <div />
--- frontend/src/components/SideBarItem/index.tsx
+++ frontend/src/components/SideBarItem/index.tsx
@@ -4,16 +4,38 @@ import { Link } from "react-router";
 type SideBarItemProps = {
   icon: LucideIcon;
   text: string;
-  to: string;
+  to?: string;
+  onClick?: () => void;
 };
 
-export function SideBarItem({ icon: Icon, text, to }: SideBarItemProps) {
-  return (
-    <li className="grid grid-cols-[40px_1fr] items-center gap-4 px-4 h-18 font-medium text-accent-foreground/80 hover:bg-accent-foreground/10 cursor-pointer transition-all hover:text-primary/70">
+export function SideBarItem({
+  icon: Icon,
+  text,
+  to,
+  onClick,
+}: SideBarItemProps) {
+  return onClick ? (
+    <button
+      onClick={onClick}
+      className="grid grid-cols-[40px_1fr] items-center gap-4 px-4 h-18 font-medium text-accent-foreground/80 hover:bg-accent-foreground/10 cursor-pointer transition-all hover:text-primary/70"
+    >
       <Icon className="h-8 w-8 justify-self-center" />
-      <Link to={to} className="text-left text-[15px]">
-        {text}
-      </Link>
-    </li>
+      <p className="text-left text-[15px]">{text}</p>
+    </button>
+  ) : (
+    <Link
+      to={to ?? "#"}
+      className="grid grid-cols-[40px_1fr] items-center gap-4 px-4 h-18 font-medium text-accent-foreground/80 hover:bg-accent-foreground/10 cursor-pointer transition-all hover:text-primary/70"
+    >
+      <Icon className="h-8 w-8 justify-self-center" />
+      <p className="text-left text-[15px]">{text}</p>
+    </Link>
   );
+  // <Link
+  //   to={to}
+  //   className="grid grid-cols-[40px_1fr] items-center gap-4 px-4 h-18 font-medium text-accent-foreground/80 hover:bg-accent-foreground/10 cursor-pointer transition-all hover:text-primary/70"
+  // >
+  //   <Icon className="h-8 w-8 justify-self-center" />
+  //   <p className="text-left text-[15px]">{text}</p>
+  // </Link>
 }
