165aef7744845070cb14d254ba0c75d1dbe3628f
luis-otavio-dias <luis777dias231@gmail.com>
2026-03-07 03:19:19 -0300

    Adiciona funcionalidade de troca de tema no menu padrão

--- frontend/src/components/DefaultMenu/index.tsx
+++ frontend/src/components/DefaultMenu/index.tsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router";
 import { Menu } from "../Menu";
-import { MenuDropdown } from "../MenuDropdown";
+import { useTheme } from "../ThemeProvider";
+import { MoonIcon, SunIcon } from "lucide-react";
 
 export function DefaultMenu() {
+  const { theme, changeTheme } = useTheme();
+
+  const NextThemeIcon = {
+    light: <MoonIcon size={32} />,
+    dark: <SunIcon size={32} />,
+    system: theme === "dark" ? <MoonIcon size={32} /> : <SunIcon size={32} />,
+  };
+
+  const handleChangeTheme = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    changeTheme();
+  };
+
   return (
     <Menu
       title={
@@ -11,7 +27,17 @@ export function DefaultMenu() {
         </Link>
       }
       actions={"search bar"}
-      children={<MenuDropdown />}
+      // children={<MenuDropdown />}
+      children={
+        <a
+          href="#"
+          aria-label="Change Theme"
+          title="Change Theme"
+          onClick={handleChangeTheme}
+        >
+          {NextThemeIcon[theme]}
+        </a>
+      }
     />
   );
 }
