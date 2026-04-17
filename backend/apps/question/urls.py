41f342b7f459ca890141b9432043ec405c3ca1d7
luis-otavio-dias <luis777dias231@gmail.com>
2026-04-16 23:04:38 -0300

    Adiciona suporte para OpenAPI schema; Atualiza views e URLs

--- backend/apps/question/urls.py
+++ backend/apps/question/urls.py
@@ -4,16 +4,16 @@ from apps.question.views import (
     ExamDetailView,
     ExamExtractionStatusView,
     ExamQuestionsView,
+    QuestionDetailView,
+    QuestionListView,
     UploadExamView,
-    get_question,
-    get_questions,
 )
 
 app_name = "question"
 
 urlpatterns = [
-    path("", get_questions, name="questions"),
-    path("<int:id>/", get_question, name="question_detail"),
+    path("", QuestionDetailView.as_view(), name="questions"),
+    path("<int:id>/", QuestionListView.as_view(), name="question_detail"),
     path("upload-exam/", UploadExamView.as_view(), name="upload_exam"),
     path(
         "tasks/<int:id>/status/",
